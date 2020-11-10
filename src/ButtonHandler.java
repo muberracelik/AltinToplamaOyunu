00787de26bd5f2a612359b9628c5526a9b6902ad
muberracelik <muberraceliik@gmail.com>
2020-11-10 15:18:04 +0300

    proje oluşturuldu

---
+++ src/ButtonHandler.java
@@ -0,0 +1,24 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mub
+ */
+class ButtonHandler implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+    Object source=e.getSource();
+    for(int i=0;i<40;i++){
+    for(int j=0;j<40;j++){
+    return;
+    }
+    }
+    }
+}
